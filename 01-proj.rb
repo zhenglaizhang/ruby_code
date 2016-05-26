c71fda61ccae57b3a0e2bf58ed04c7d92c41f4f9
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2016-05-26 16:33:58 +0800

    Add basic ruby scripts

---
+++ 01-proj.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+# How to run the file
+# 1. ruby 01-proj.rb
+# 2. chmod u+x 01-proj.rb
+#    ./01-proj.rb
+puts "Hello Ruby"
+puts "Now is #{Time.now}"
+
+
+# How to get doc of ruby
+# >ri ClassName
+# >ri GC
+# >ri GC::enable
---
+++ 02-new.rb
@@ -0,0 +1,86 @@
+#!/usr/bin/env ruby
+
+puts "my length is".length
+puts "the index is".index('h');
+puts 42.even? 
+puts 12.abs
+
+
+num = -12
+# Java: Math.abs(num);
+puts num.abs
+
+
+#def say_goodnight(name)
+def say_goodnight name
+    "Good night, #{name.capitalize}"
+    # result = "Good night, #{name.capitalize}"
+    # return result
+end
+
+puts say_goodnight("zhenglai")
+puts(say_goodnight("Junlai"))
+
+
+# string literal
+puts 'hello \nnot another line'
+puts "hello \nanother line"
+
+
+
+
+# Array and Hash
+
+a = [1, 2, "3", 4, 5.01]
+puts a[2]
+puts "The last element: #{a[a.length-1]}"
+
+a[0] = nil
+puts "The array now is: #{a.inspect}"
+
+
+a = [ "hello1", "hello2", "hello3" ]
+puts a.inspect
+b = %w{ ant bee cat dog }
+puts b.inspect
+
+h = {
+    "name" => "Zhenglai Zhang",
+    "age"  => 27
+}
+
+p h
+p h['name']
+p h['none']
+
+
+histogram = Hash.new(0)
+p histogram['ruby']
+
+
+# Symbols
+NORTH = 1
+SOUTH = 2
+EAST  = 3
+WEST  = 4
+
+
+def walk(direction)
+    if direction == :north
+        p "go to north"
+    end
+end
+
+walk(:north)
+h2 = {
+    :hero => "hero_value",
+    man: "man_value"
+}
+p h2.inspect
+
+large = 14
+puts "large" if large > 12
+
+square = 4
+square = square**2 while square < 1000
+p square
